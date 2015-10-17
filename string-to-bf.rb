3c51b6615fd8939cc8325e7a9c2507d59e61b986
TrevorFoster <trevor.foster@hotmail.ca>
2015-10-17 03:03:07 -0400

    Switched to spaces.

--- string-to-bf.rb
+++ string-to-bf.rb
@@ -1,83 +1,83 @@
 
 def factor(n)
-	factors = []
-	for i in (2..n / 2).reverse_each
-		if n % i == 0
-			factors << [i, n / i]
-		end
-	end
+    factors = []
+    for i in (2..n / 2).reverse_each
+        if n % i == 0
+            factors << [i, n / i]
+        end
+    end
 
-	return factors[factors.length / 2]
+    return factors[factors.length / 2]
 end
 
 def isprime(n)
-	return true if n == 2
+    return true if n == 2
 
-	for i in (2..(n / 2 + 1))
-		return false if n % i == 0
-	end
+    for i in (2..(n / 2 + 1))
+        return false if n % i == 0
+    end
 
-	return true
+    return true
 end
 
 def closestComposite(n)
-	i = 1
-	while 1
-		if !isprime(n + i)
-			return n + i
-		elsif !isprime(n - i)
-			return n - i
-		end
-		i += 1
-	end
+    i = 1
+    while 1
+        if !isprime(n + i)
+            return n + i
+        elsif !isprime(n - i)
+            return n - i
+        end
+        i += 1
+    end
 end
 
 def translate(message)
-	p = 0
-	buffer = Array.new(1000){0}
-	compiled = ""
+    p = 0
+    buffer = Array.new(1000){0}
+    compiled = ""
 
-	message.each_byte do |c|
-		if buffer[p] == c
-			compiled += '.'
-			buffer[p] = c
-			next
-		end
+    message.each_byte do |c|
+        if buffer[p] == c
+            compiled += '.'
+            buffer[p] = c
+            next
+        end
 
-		left, diff = c - buffer[p], 0
+        left, diff = c - buffer[p], 0
 
-		if isprime(left.abs)
-			if left.abs < 4
-				compiled += ("++-"[left <=> 0]) * left.abs + "."
-				buffer[p] = c
-				next
-			end
+        if isprime(left.abs)
+            if left.abs < 4
+                compiled += ("++-"[left <=> 0]) * left.abs + "."
+                buffer[p] = c
+                next
+            end
 
-			newLeft = (left <=> 0) * closestComposite(left.abs)
-			diff = (newLeft - left)
-			left = newLeft
-		end
+            newLeft = (left <=> 0) * closestComposite(left.abs)
+            diff = (newLeft - left)
+            left = newLeft
+        end
 
-		i, j = factor(left.abs)
+        i, j = factor(left.abs)
 
-		if p != 0
-			compiled += "<" * p
-			p = 0
-		end
+        if p != 0
+            compiled += "<" * p
+            p = 0
+        end
 
-		compiled += "+" * i
-		compiled += "[>" + ("++-"[left <=> 0]) * j + "<-]>" + ("++-"[-diff <=> 0]) * diff.abs + "."
+        compiled += "+" * i
+        compiled += "[>" + ("++-"[left <=> 0]) * j + "<-]>" + ("++-"[-diff <=> 0]) * diff.abs + "."
 
-		p += 1
-		buffer[p] = c
-	end
+        p += 1
+        buffer[p] = c
+    end
 
-	return compiled
+    return compiled
 end
 
 if ARGV.length < 1
-	puts "Please pass a string as an argument"
-	exit
+    puts "Please pass a string as an argument"
+    exit
 end
 
 puts translate(ARGV[0])
